cdf01271c72ee4a795f9fb4ead59d021f00f8a6a
sulmi24 <a.sulmi24@gmail.com>
2023-03-11 21:48:35 +0200

    completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+let main = document.getElementById("main");
+document.body.removeChild(main);
+
+let newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Yassin is the champion";
